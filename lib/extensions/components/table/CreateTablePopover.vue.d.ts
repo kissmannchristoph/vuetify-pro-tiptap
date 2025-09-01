72b76a3f7a2ef7574531217b4b79f3e8bdbe9290
Christoph <kissmannchristoph@gmail.com>
2025-09-01 20:42:03 +0200

    fix(lib): add lib

---
+++ lib/extensions/components/table/CreateTablePopover.vue.d.ts
@@ -0,0 +1,7 @@
+import { CreateTablePayload } from './type';
+declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    "create-table": (payload: CreateTablePayload) => void;
+}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{
+    "onCreate-table"?: ((payload: CreateTablePayload) => any) | undefined;
+}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+export default _default;
---
+++ lib/install.d.ts
@@ -0,0 +1,10 @@
+import { Extensions } from '@tiptap/core';
+import { Plugin } from 'vue';
+export interface InstallationOptions {
+    lang?: string;
+    fallbackLang?: string;
+    markdownTheme?: string;
+    components?: Record<string, any>;
+    extensions?: Extensions;
+}
+export declare const createVuetifyProTipTap: (opts: InstallationOptions) => Plugin;
